4cb87b3240093479237262046e5443ee48f961ba
Holden Cormier <hcormie2@villanova.edu>
2023-09-12 16:07:43 -0400

    adding employee objects

---
+++ src/main/java/com/ems/database/models/Employee.java
@@ -0,0 +1,27 @@
+package com.ems.database.models;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "employees")
+public class Employee {
+
+    @Id
+    private ObjectId employeeId;
+
+    @Field
+    private String employeeName;
+
+    public Employee() {
+    }
+
+    public Employee(ObjectId employeeId, String employeeName) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+
+
+}
+
---
+++ src/main/java/com/ems/database/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.ems.database.repositories;
+
+import com.ems.database.models.Employee;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+@Document(collection = "employees")
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+}
