61347282c0d020a8a7183bf376ce55d706b79090
Holden Cormier <hcormie2@villanova.edu>
2023-09-14 21:26:35 -0400

    Revert "typo fix"
    This reverts commit 26c9c15ac9602ef94ecd6e21c93e71207a548c5f.

--- src/main/java/com/ems/services/DatabaseServices.java
+++ src/main/java/com/ems/services/DatabaseServices.java
@@ -10,7 +10,7 @@ public class DatabaseServices {
 
     // find employee by id
     public static Optional<Employee> findEmployeeById(ObjectId employeeId) {
-        return EmsApplication.visibleEmployeeRepository
+        return EmsApplication.visableEmployeeRepository
                 .findAll()
                 .stream()
                 .filter(employee -> employee.getEmployeeId()
@@ -20,7 +20,7 @@ public class DatabaseServices {
 
     // find location by id
     public static Optional<Location> findLocationById(ObjectId locationId) {
-        return EmsApplication.visibleLocationRepository
+        return EmsApplication.visableLocationRepository
                 .findAll()
                 .stream()
                 .filter(location -> location.getLocationId()
@@ -30,7 +30,7 @@ public class DatabaseServices {
 
     // find manager by id
     public static Optional<Manager> findManagerById(ObjectId managerId) {
-        return EmsApplication.visibleManagerRepository
+        return EmsApplication.visableManagerRepository
                 .findAll()
                 .stream()
                 .filter(manager -> manager.getManagerId()
@@ -40,7 +40,7 @@ public class DatabaseServices {
 
     // find organization by id
     public static Optional<Organization> findOrganizationById(ObjectId organizationId) {
-        return EmsApplication.visibleOrganizationRepository
+        return EmsApplication.visableOrganizationRepository
                 .findAll()
                 .stream()
                 .filter(organization -> organization.getOrganizationId()
@@ -50,7 +50,7 @@ public class DatabaseServices {
 
     // find shift by id
     public static Optional<Shift> findShiftById(ObjectId shiftId) {
-        return EmsApplication.visibleShiftRepository
+        return EmsApplication.visableShiftRepository
                 .findAll()
                 .stream()
                 .filter(shift -> shift.getShiftId()
@@ -60,76 +60,76 @@ public class DatabaseServices {
 
     // save employee
     public static void saveEmployee(Employee employee) {
-        EmsApplication.visibleEmployeeRepository.save(employee);
+        EmsApplication.visableEmployeeRepository.save(employee);
     }
 
     // save location
     public static void saveLocation(Location location) {
-        EmsApplication.visibleLocationRepository.save(location);
+        EmsApplication.visableLocationRepository.save(location);
     }
 
     // save manager
     public static void saveManager(Manager manager) {
-        EmsApplication.visibleManagerRepository.save(manager);
+        EmsApplication.visableManagerRepository.save(manager);
     }
 
     // save organization
     public static void saveOrganization(Organization organization) {
-        EmsApplication.visibleOrganizationRepository.save(organization);
+        EmsApplication.visableOrganizationRepository.save(organization);
     }
 
     // save shift
     public static void saveShift(Shift shift) {
-        EmsApplication.visibleShiftRepository.save(shift);
+        EmsApplication.visableShiftRepository.save(shift);
     }
 
     // delete employee
     public static void deleteEmployee(Employee employee) {
         ObjectId employeeId = employee.getEmployeeId();
-        if (EmsApplication.visibleEmployeeRepository.findAll().stream().anyMatch(em -> em.getEmployeeId().equals(employeeId))){
+        if (EmsApplication.visableEmployeeRepository.findAll().stream().anyMatch(em -> em.getEmployeeId().equals(employeeId))){
             System.out.println("Employee not found");
             throw new RuntimeException("Error deleting employee! Employee with id: " + employeeId + " is not present in the database");
         }
-        EmsApplication.visibleEmployeeRepository.delete(employee);
+        EmsApplication.visableEmployeeRepository.delete(employee);
     }
 
     // delete location
     public static void deleteLocation(Location location) {
         ObjectId locationId = location.getLocationId();
-        if (EmsApplication.visibleLocationRepository.findAll().stream().anyMatch(lo -> lo.getLocationId().equals(locationId))){
+        if (EmsApplication.visableLocationRepository.findAll().stream().anyMatch(lo -> lo.getLocationId().equals(locationId))){
             System.out.println("Location not found");
             throw new RuntimeException("Error deleting location! Location with id: " + locationId + " is not present in the database");
         }
-        EmsApplication.visibleLocationRepository.delete(location);
+        EmsApplication.visableLocationRepository.delete(location);
     }
 
     // delete manager
     public static void deleteManager(Manager manager) {
         ObjectId managerId = manager.getManagerId();
-        if (EmsApplication.visibleManagerRepository.findAll().stream().anyMatch(ma -> ma.getManagerId().equals(managerId))){
+        if (EmsApplication.visableManagerRepository.findAll().stream().anyMatch(ma -> ma.getManagerId().equals(managerId))){
             System.out.println("Manager not found");
             throw new RuntimeException("Error deleting manager! Manager with id: " + managerId + " is not present in the database");
         }
-        EmsApplication.visibleManagerRepository.delete(manager);
+        EmsApplication.visableManagerRepository.delete(manager);
     }
 
     // delete organization
     public static void deleteOrganization(Organization organization) {
         ObjectId organizationId = organization.getOrganizationId();
-        if (EmsApplication.visibleOrganizationRepository.findAll().stream().anyMatch(or -> or.getOrganizationId().equals(organizationId))){
+        if (EmsApplication.visableOrganizationRepository.findAll().stream().anyMatch(or -> or.getOrganizationId().equals(organizationId))){
             System.out.println("Organization not found");
             throw new RuntimeException("Error deleting organization! Organization with id: " + organizationId + " is not present in the database");
         }
-        EmsApplication.visibleOrganizationRepository.delete(organization);
+        EmsApplication.visableOrganizationRepository.delete(organization);
     }
 
     // delete shift
     public static void deleteShift(Shift shift) {
         ObjectId shiftId = shift.getShiftId();
-        if (EmsApplication.visibleShiftRepository.findAll().stream().anyMatch(sh -> sh.getShiftId().equals(shiftId))){
+        if (EmsApplication.visableShiftRepository.findAll().stream().anyMatch(sh -> sh.getShiftId().equals(shiftId))){
             System.out.println("Shift not found");
             throw new RuntimeException("Error deleting shift! Shift with id: " + shiftId + " is not present in the database");
         }
-        EmsApplication.visibleShiftRepository.delete(shift);
+        EmsApplication.visableShiftRepository.delete(shift);
     }
 }
