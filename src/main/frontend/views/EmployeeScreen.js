da327a9e494003da996fb8c272c14324e25da85b
terrence-nolan <terrencenolan@protonmail.com>
2023-09-21 14:56:07 -0400

    scroll view stretches whole screen

--- src/main/frontend/views/EmployeeScreen.js
+++ src/main/frontend/views/EmployeeScreen.js
@@ -8,7 +8,7 @@ function EmployeeScreen() {
     return (
         <View style={styles.screen}>
             <CustomHeader title={"Employee Name"} page={ScreenNames.EMPLOYEE_SETTINGS} />
-            <ScrollView>
+            <ScrollView style={styles.scrollView}>
                 <EmployeeShiftCard date={"Wed Sep 21"} shiftType={"Lifeguard"} startTime={"12:00"} endTime={"8:30"} locationId={12345} />
             </ScrollView>
         </View>
@@ -19,7 +19,13 @@ function EmployeeScreen() {
 const styles = StyleSheet.create({
     screen: {
         backgroundColor: '#F1F1F1',
-    }
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+    },
+    scrollView: {
+        flexGrow: 1,
+    },
 });
 
 export default EmployeeScreen;
