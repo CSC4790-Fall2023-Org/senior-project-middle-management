39ab2df598dd56b488d349657e2cfe91d267a2a9
terrence-nolan <terrencenolan@protonmail.com>
2023-09-22 13:57:46 -0400

    refactor code

--- src/main/frontend/views/EmployeeScreen.js
+++ src/main/frontend/views/EmployeeScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View} from 'react-native';
 import CustomHeader from "../components/CustomHeader";
-import EmployeeShifts from "../components/EmployeeShifts";
-import SwiperComponent from "../components/SwiperComponent";
+import EmployeeShiftHeader from "../components/EmployeeShiftHeader";
 
 function EmployeeScreen() {
     return (
@@ -11,26 +10,10 @@ function EmployeeScreen() {
                 <CustomHeader title={"Employee Name"} />
             </View>
             <View>
-                <EmployeeShifts />
-                {/*<SwiperComponent />*/}
+                <EmployeeShiftHeader />
             </View>
         </View>
-
     );
-
 }
 
-const styles = StyleSheet.create({
-    buttonsContainer: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        paddingTop: 200,
-    },
-    loginButton: {
-        width: 200,
-        margin: 0,
-
-    },
-});
 export default EmployeeScreen;
