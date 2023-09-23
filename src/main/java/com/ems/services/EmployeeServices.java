fad1f65b7a33f75b4ae607f422f9f8142bed3eaa
rgatdul1 <rgatdul1@villanova.edu>
2023-09-22 20:08:54 -0400

    updated employee svc

--- src/main/java/com/ems/services/EmployeeServices.java
+++ src/main/java/com/ems/services/EmployeeServices.java
@@ -1,4 +1,5 @@
 package com.ems.services;
+
 import com.ems.Exceptions.DatabaseException;
 import com.ems.Exceptions.SvcException;
 import com.ems.database.models.Employee;
@@ -6,32 +7,45 @@ import com.ems.database.models.Organization;
 import com.ems.database.models.Shift;
 import org.bson.types.ObjectId;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeServices {
-    public static Object[] assignShiftToEmployeeUsingIDS(final ObjectId pEmployeeID, final ObjectId pShiftID) throws SvcException, DatabaseException{
-        // create necessary objects using IDs
-        Employee employee = DatabaseServices.findEmployeeById(pEmployeeID)
-                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_EMPLOYEE, pEmployeeID));
-        Shift shift = DatabaseServices.findShiftById(pShiftID)
-                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_SHIFT, pShiftID));
-        Organization organization = DatabaseServices.findOrganizationById(employee.getOrganizationId())
-                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_ORGANIZATION, employee.getOrganizationId()));
 
+    public static Object[] assignShiftToEmployeeUsingIDS(ObjectId pEmployeeID, ObjectId pShiftID) throws SvcException, DatabaseException {
+        // Retrieve necessary objects using IDs
+        Employee employee = findEmployeeById(pEmployeeID);
+        Shift shift = findShiftById(pShiftID);
+        Organization organization = findOrganizationById(employee.getOrganizationId());
+
+        // Delegate to the overloaded method
         return assignShiftToEmployeeUsingIDS(employee, shift, organization);
     }
 
-    public static Object[] assignShiftToEmployeeUsingIDS(final Employee pEmployee, final Shift pShift, final Organization pOrganization) throws SvcException {
-        // validation
+    private static Employee findEmployeeById(ObjectId employeeID) throws DatabaseException {
+        return DatabaseServices.findEmployeeById(employeeID)
+                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_EMPLOYEE, employeeID));
+    }
+
+    private static Shift findShiftById(ObjectId shiftID) throws DatabaseException {
+        return DatabaseServices.findShiftById(shiftID)
+                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_SHIFT, shiftID));
+    }
+
+    private static Organization findOrganizationById(ObjectId organizationID) throws DatabaseException {
+        return DatabaseServices.findOrganizationById(organizationID)
+                .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_ORGANIZATION, organizationID));
+    }
+
+    public static Object[] assignShiftToEmployeeUsingIDS(Employee pEmployee, Shift pShift, Organization pOrganization) throws SvcException {
+        // Validation
         ValidationServices.validateEmployeeCanConnectToShift(pEmployee, pShift, pOrganization);
 
-        // adding the shift to the employee and changing its status
-        List<ObjectId> resultList = new ArrayList<>(List.copyOf(pEmployee.getShiftIdList()));
+        // Adding the shift to the employee and changing its status
+        List<ObjectId> resultList = pEmployee.getShiftIdList();
         resultList.add(pShift.getShiftId());
         pEmployee.setShiftIdList(resultList);
         pShift.setShiftOpen(false);
+
         return new Object[]{pEmployee, pShift};
     }
-
 }
