58dc35ef74ebc96c2876adbc8fea9b9197ad94f7
rgatdul1 <rgatdul1@villanova.edu>
2023-09-25 17:29:49 -0400

    updated employee svc

--- src/main/java/com/ems/services/EmployeeServices.java
+++ src/main/java/com/ems/services/EmployeeServices.java
@@ -12,12 +12,9 @@ import java.util.List;
 public class EmployeeServices {
 
     public static Object[] assignShiftToEmployeeUsingIDS(ObjectId pEmployeeID, ObjectId pShiftID) throws SvcException, DatabaseException {
-        // Retrieve necessary objects using IDs
         Employee employee = findEmployeeById(pEmployeeID);
         Shift shift = findShiftById(pShiftID);
         Organization organization = findOrganizationById(employee.getOrganizationId());
-
-        // Delegate to the overloaded method
         return assignShiftToEmployeeUsingIDS(employee, shift, organization);
     }
 
@@ -37,15 +34,11 @@ public class EmployeeServices {
     }
 
     public static Object[] assignShiftToEmployeeUsingIDS(Employee pEmployee, Shift pShift, Organization pOrganization) throws SvcException {
-        // Validation
         ValidationServices.validateEmployeeCanConnectToShift(pEmployee, pShift, pOrganization);
-
-        // Adding the shift to the employee and changing its status
         List<ObjectId> resultList = pEmployee.getShiftIdList();
         resultList.add(pShift.getShiftId());
         pEmployee.setShiftIdList(resultList);
         pShift.setShiftOpen(false);
-
         return new Object[]{pEmployee, pShift};
     }
 }
