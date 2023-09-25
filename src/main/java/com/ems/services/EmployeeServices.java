c93bc0b4953887c587f6db0a3e28405556256eec
rgatdul1 <rgatdul1@villanova.edu>
2023-09-25 19:25:09 -0400

    updated employee svc and employee svc tests

--- src/main/java/com/ems/services/EmployeeServices.java
+++ src/main/java/com/ems/services/EmployeeServices.java
@@ -11,30 +11,40 @@ import java.util.List;
 
 public class EmployeeServices {
 
-    public static Object[] assignShiftToEmployeeUsingIDS(ObjectId pEmployeeID, ObjectId pShiftID) throws SvcException, DatabaseException {
+    private final DatabaseServices databaseServices;
+    private final ValidationServices validationServices;
+
+    public EmployeeServices(DatabaseServices databaseServices, ValidationServices validationServices) {
+        this.databaseServices = databaseServices;
+        this.validationServices = validationServices;
+    }
+
+    public Object[] assignShiftToEmployeeUsingIDS(ObjectId pEmployeeID, ObjectId pShiftID)
+            throws SvcException, DatabaseException {
         Employee employee = findEmployeeById(pEmployeeID);
         Shift shift = findShiftById(pShiftID);
         Organization organization = findOrganizationById(employee.getOrganizationId());
         return assignShiftToEmployeeUsingIDS(employee, shift, organization);
     }
 
-    private static Employee findEmployeeById(ObjectId employeeID) throws DatabaseException {
-        return DatabaseServices.findEmployeeById(employeeID)
+    private Employee findEmployeeById(ObjectId employeeID) throws DatabaseException {
+        return databaseServices.findEmployeeById(employeeID)
                 .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_EMPLOYEE, employeeID));
     }
 
-    private static Shift findShiftById(ObjectId shiftID) throws DatabaseException {
-        return DatabaseServices.findShiftById(shiftID)
+    private Shift findShiftById(ObjectId shiftID) throws DatabaseException {
+        return databaseServices.findShiftById(shiftID)
                 .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_SHIFT, shiftID));
     }
 
-    private static Organization findOrganizationById(ObjectId organizationID) throws DatabaseException {
-        return DatabaseServices.findOrganizationById(organizationID)
+    private Organization findOrganizationById(ObjectId organizationID) throws DatabaseException {
+        return databaseServices.findOrganizationById(organizationID)
                 .orElseThrow(() -> new DatabaseException(DatabaseException.LOCATING_ORGANIZATION, organizationID));
     }
 
-    public static Object[] assignShiftToEmployeeUsingIDS(Employee pEmployee, Shift pShift, Organization pOrganization) throws SvcException {
-        ValidationServices.validateEmployeeCanConnectToShift(pEmployee, pShift, pOrganization);
+    public Object[] assignShiftToEmployeeUsingIDS(Employee pEmployee, Shift pShift, Organization pOrganization)
+            throws SvcException {
+        validationServices.validateEmployeeCanConnectToShift(pEmployee, pShift, pOrganization);
         List<ObjectId> resultList = pEmployee.getShiftIdList();
         resultList.add(pShift.getShiftId());
         pEmployee.setShiftIdList(resultList);
--- src/test/java/com/ems/ServicesTests/EmployeeServicesTests.java
+++ src/test/java/com/ems/ServicesTests/EmployeeServicesTests.java
@@ -1,4 +1,4 @@
-package com.ems.ServicesTests;
+package com.ems.services; // Adjust the package name to match the tested class
 
 import com.ems.Exceptions.SvcException;
 import com.ems.Utils.EmployeeUtils;
@@ -7,28 +7,49 @@ import com.ems.Utils.ShiftUtils;
 import com.ems.database.models.Employee;
 import com.ems.database.models.Organization;
 import com.ems.database.models.Shift;
-import com.ems.services.EmployeeServices;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class EmployeeServicesTests {
+
+    private DatabaseServices databaseServices;
+    private ValidationServices validationServices;
+    private EmployeeServices employeeServices;
+
+    @BeforeEach
+    public void setUp() {
+        databaseServices = mock(DatabaseServices.class);
+        validationServices = mock(ValidationServices.class);
+        employeeServices = new EmployeeServices(databaseServices, validationServices);
+    }
+
     @Test
     public void testSetShiftToEmployeeUsingIDS() throws SvcException {
         Employee employee = EmployeeUtils.getBaseEmployee();
         Shift shift = ShiftUtils.getBaseShift();
         Organization organization = OrganizationUtils.getBaseOrganization();
 
-        {
-            // valid employee, shift, and org
-            Object[] result = EmployeeServices.assignShiftToEmployeeUsingIDS(employee,shift,organization);
-            Employee resultEmployee = (Employee) result[0];
-            assertEquals(List.of(employee.getShiftIdList().get(0), shift.getShiftId()), resultEmployee.getShiftIdList());
-            assertFalse(shift.isShiftOpen());
-        }
+        when(databaseServices.findEmployeeById(any())).thenReturn(java.util.Optional.of(employee));
+        when(databaseServices.findShiftById(any())).thenReturn(java.util.Optional.of(shift));
+        when(databaseServices.findOrganizationById(any())).thenReturn(java.util.Optional.of(organization));
+        doNothing().when(validationServices).validateEmployeeCanConnectToShift(employee, shift, organization);
+
+        Object[] result = employeeServices.assignShiftToEmployeeUsingIDS(employee, shift, organization);
+        Employee resultEmployee = (Employee) result[0];
+
+        assertEquals(List.of(employee.getShiftIdList().get(0), shift.getShiftId()), resultEmployee.getShiftIdList());
+        assertFalse(shift.isShiftOpen());
+
+        verify(validationServices, times(1)).validateEmployeeCanConnectToShift(employee, shift, organization);
+        verify(databaseServices, times(1)).findEmployeeById(any());
+        verify(databaseServices, times(1)).findShiftById(any());
+        verify(databaseServices, times(1)).findOrganizationById(any());
     }
 }
