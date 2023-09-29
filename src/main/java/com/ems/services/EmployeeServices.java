fef5460f0999aa2f87c32131fd012c07d210479d
Holden Cormier <hcormie2@villanova.edu>
2023-09-28 20:11:22 -0400

    added backend method to create employee

--- src/main/java/com/ems/services/EmployeeServices.java
+++ src/main/java/com/ems/services/EmployeeServices.java
@@ -1,11 +1,18 @@
 package com.ems.services;
 import com.ems.Exceptions.DatabaseException;
 import com.ems.Exceptions.SvcException;
+import com.ems.Utils.JsonUtils;
 import com.ems.database.models.Employee;
 import com.ems.database.models.Organization;
 import com.ems.database.models.Shift;
+import com.sun.jdi.ObjectCollectedException;
 import org.bson.types.ObjectId;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +39,33 @@ public class EmployeeServices {
         pEmployee.setShiftIdList(resultList);
         pShift.setShiftOpen(false);
         return new Object[]{pEmployee, pShift};
+    }
+
+    public static ResponseEntity createEmployee(final HttpMethod pMethod, final URI pUrl, final String pBody) {
+        // todo: figure out what method and url mean
+
+        Employee employee;
+        try{
+            employee = JsonUtils.getEmployeeFromJSON(new JSONObject(pBody));
+        } catch (SvcException | JSONException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(400).body("Error creating employee from JSON");
+
+        }
+
+        final Employee finalEmployee = employee;
+        if (DatabaseServices.getAllEmployees().stream().anyMatch(e -> e.equals(finalEmployee))){
+            return ResponseEntity.status(400).body("Employee already exists");
+        }
+
+        try{
+            DatabaseServices.saveEmployee(employee);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(400).body("Error saving employee to database");
+        }
+
+        return ResponseEntity.status(200).body("Employee created successfully");
     }
 
 }
