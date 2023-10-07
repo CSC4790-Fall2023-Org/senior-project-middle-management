8d6bef451151f94aa0f009d22b6d9e49aed63759
Diego Messmacher Montes de Oca <diegomess1908@gmail.com>
2023-10-07 16:26:51 -0400

    consolidated files, fixed add employee

--- src/main/frontend/views/EmployeeScreen.js
+++ src/main/frontend/views/EmployeeScreen.js
@@ -3,9 +3,9 @@ import {View, StyleSheet} from 'react-native';
 import CustomHeader from "../components/CustomHeader";
 import {ScreenNames} from "../utils/ScreenNames";
 import EmployeeHrsStatusBar from "../components/EmployeeHrsStatusBar";
-import EmployeeShiftHeader from "../components/EmployeeShiftHeader";
 import MyShiftList from "../components/MyShiftList";
 import AvailableShiftList from "../components/AvailableShiftList";
+import CustomDashboardHeader from "../components/CustomDashboardHeader";
 
 function EmployeeScreen() {
     const [selectedIndex, setSelectedIndex] = useState(0);
@@ -14,10 +14,21 @@ function EmployeeScreen() {
         setSelectedIndex(index);
     }
 
+    const tabs = [
+        {
+            id: 1,
+            text: 'My Shifts',
+        },
+        {
+            id: 2,
+            text: 'Available',
+        },
+    ];
+
     return (
         <View style={styles.screen}>
             <CustomHeader title={"Employee Name"} page={ScreenNames.EMPLOYEE_SETTINGS} />
-            <EmployeeShiftHeader onTitlePress={handleTitlePress}/>
+            <CustomDashboardHeader onTitlePress={handleTitlePress} tabs={tabs}/>
             {selectedIndex === 0 && <MyShiftList />}
             {selectedIndex === 1 && <AvailableShiftList />}
             <EmployeeHrsStatusBar loggedHours={20} maxWorkableHours={40} style={styles.statusBar}/>
