765d35c9d523d6b3f9d16e82a3990c6a835f7da4
Holden Cormier <hcormie2@villanova.edu>
2023-10-30 16:46:17 -0400

    getCorrectDateFormatFromLocalDateTime

--- src/main/java/com/ems/Utils/DateUtils.java
+++ src/main/java/com/ems/Utils/DateUtils.java
@@ -1,9 +1,16 @@
 package com.ems.Utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class DateUtils {
     public static double getHoursBetweenShifts(final LocalDateTime pStartDate, final LocalDateTime pEndDate){
         return (pEndDate.getHour() - pStartDate.getHour()) + ((pEndDate.getMinute() - pStartDate.getMinute()) / 60.0);
     }
+
+    public static String getCorrectDateFormatFromLocalDateTime(final LocalDateTime pDate){
+        return pDate.getDayOfWeek().name().substring(0, 3)+ "" + pDate.getMonth().name().substring(0,3) + " " + pDate.getDayOfMonth();
+    }
+
+
 }
