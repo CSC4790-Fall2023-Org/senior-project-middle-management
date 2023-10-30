af849e83e6bbcfe5dbec43458a2e0f74cea418fe
Holden Cormier <hcormie2@villanova.edu>
2023-10-30 17:08:23 -0400

    convert from military to user friendly time

--- src/main/java/com/ems/Utils/DateUtils.java
+++ src/main/java/com/ems/Utils/DateUtils.java
@@ -1,5 +1,6 @@
 package com.ems.Utils;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -11,6 +12,22 @@ public class DateUtils {
     public static String getCorrectDateFormatFromLocalDateTime(final LocalDateTime pDate){
         return pDate.getDayOfWeek().name().substring(0, 3)+ "" + pDate.getMonth().name().substring(0,3) + " " + pDate.getDayOfMonth();
     }
+
+    public static String getCorrectTimeFromLocalDateTime(final LocalDateTime pDate){
+        String isAMORPM = pDate.getHour() < 12 ? "am" : "pm";
+//        String
+        return pDate.getHour() + ":" + pDate.getMinute() + "" + isAMORPM;
+    }
+
+
+    public static String convertFromMilitaryTimeToUsable(final LocalDateTime pDate){
+        final DecimalFormat df = new DecimalFormat("#00");
+        String isAMORPM = pDate.getHour() < 12 ? "am" : "pm";
+        String hour = String.valueOf(pDate.getHour() < 12 ? pDate.getHour() : pDate.getHour() - 12);
+        hour = hour.equals("0") ? "12" : hour;
+        return hour + ":" + df.format(pDate.getMinute()) + "" + isAMORPM;
+    }
+
 
 
 }
--- src/test/java/com/ems/UtilsTests/DateUtilsTests.java
+++ src/test/java/com/ems/UtilsTests/DateUtilsTests.java
@@ -26,4 +26,38 @@ public class DateUtilsTests {
         }
     }
 
+    @Test
+    public void testConvertFromMilitaryToUsable(){
+        {
+            // 10AM
+            final LocalDateTime time = LocalDateTime.of(2023, 1, 1, 10, 0);
+            final String convertedTime = com.ems.Utils.DateUtils.convertFromMilitaryTimeToUsable(time);
+            assertEquals("10:00am", convertedTime);
+        }
+        {
+            // 12AM
+            final LocalDateTime time = LocalDateTime.of(2023, 1, 1, 0, 0);
+            final String convertedTime = com.ems.Utils.DateUtils.convertFromMilitaryTimeToUsable(time);
+            assertEquals("12:00am", convertedTime);
+        }
+        {
+            // 12PM
+            final LocalDateTime time = LocalDateTime.of(2023, 1, 1, 12, 0);
+            final String convertedTime = com.ems.Utils.DateUtils.convertFromMilitaryTimeToUsable(time);
+            assertEquals("12:00pm", convertedTime);
+        }
+        {
+            // 10:30AM
+            final LocalDateTime time = LocalDateTime.of(2023, 1, 1, 10, 30);
+            final String convertedTime = com.ems.Utils.DateUtils.convertFromMilitaryTimeToUsable(time);
+            assertEquals("10:30am", convertedTime);
+        }
+        {
+            // 3:30PM
+            final LocalDateTime time = LocalDateTime.of(2023, 1, 1, 15, 30);
+            final String convertedTime = com.ems.Utils.DateUtils.convertFromMilitaryTimeToUsable(time);
+            assertEquals("3:30pm", convertedTime);
+        }
+    }
+
 }
