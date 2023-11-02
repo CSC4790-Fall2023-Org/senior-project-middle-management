f31246092684a9ed66730e0a52500f148ee167a6
Holden Cormier <hcormie2@villanova.edu>
2023-11-01 20:56:56 -0400

    API endpoint to create location

---
+++ src/main/java/com/ems/controllers/LocationController.java
@@ -0,0 +1,18 @@
+package com.ems.controllers;
+
+import com.ems.Utils.LocationUtils;
+import com.ems.services.LocationServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+
+    @PostMapping("/createLocation")
+    public ResponseEntity createLocation(@RequestBody final String pPayload) {
+        return LocationServices.createLocation(pPayload);
+    }
+
+}
