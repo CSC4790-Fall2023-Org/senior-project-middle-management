e2e28ab87b3e17fcd88e4ad49867c8f1cd125d34
terrence-nolan <terrencenolan@protonmail.com>
2023-11-27 18:54:54 -0500

    company page font sizes

--- src/main/frontend/components/company-dashboard-components/StaffBanner.js
+++ src/main/frontend/components/company-dashboard-components/StaffBanner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-
+import {grayBackground, primaryGreen, white} from "../../utils/Colors";
+import CustomButton from "../CustomButton";
 
 const StaffBanner = ({setBannerPress, bannerPress}) => {
     const handlePressRight = () => {
@@ -12,12 +13,22 @@ const StaffBanner = ({setBannerPress, bannerPress}) => {
     return(
         <View style={styles.container}>
             <TouchableOpacity onPress={handlePressLeft}>
-                <View style={[!bannerPress ? {backgroundColor:'#50C878'}:{backgroundColor:'#FFFFFF'}, styles.touchableContainer, {borderTopLeftRadius:15,borderBottomLeftRadius:15,}]}>
-                    <Text style={!bannerPress ? styles.touchedText:styles.text}>Employees</Text>
+                <View
+                    style={[!bannerPress ? {backgroundColor: primaryGreen}
+                        : {backgroundColor: grayBackground},
+                        styles.touchableContainer,
+                        {borderTopLeftRadius: 15, borderBottomLeftRadius: 15}]}
+                >
+                    <Text style={!bannerPress ? styles.touchedText : styles.text}>Employees</Text>
                 </View>
             </TouchableOpacity>
             <TouchableOpacity onPress={handlePressRight}>
-                <View style={[bannerPress ? {backgroundColor:'#50C878'}:{backgroundColor:'#FFFFFF'},styles.touchableContainer, {borderTopRightRadius:15,borderBottomRightRadius:15,}]}>
+                <View
+                    style={[bannerPress ? {backgroundColor : primaryGreen}
+                        : {backgroundColor: grayBackground},
+                        styles.touchableContainer,
+                        {borderTopRightRadius: 15, borderBottomRightRadius: 15}]}
+                >
                     <Text style={bannerPress ? styles.touchedText:styles.text}>Managers</Text>
                 </View>
             </TouchableOpacity>
@@ -26,29 +37,29 @@ const StaffBanner = ({setBannerPress, bannerPress}) => {
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    touchableContainer:{
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        paddingVertical:10,
-        paddingHorizontal:20,
-        width:150,
-        borderWidth:1,
-        borderColor:'#50C878',
+    touchableContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: CustomButton.width,
+        borderWidth: 1,
+        borderColor: primaryGreen,
     },
-    text:{
-        color:'#50C878',
-        fontSize:20,
+    text: {
+        color: primaryGreen,
+        fontSize: 17,
     },
-    touchedText:{
-        color:'#FFFFFF',
-        fontSize:20,
+    touchedText: {
+        color: white,
+        fontSize: 17,
     },
 });
+
 export default StaffBanner;
